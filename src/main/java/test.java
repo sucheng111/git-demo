dd0561d8dd0d17d2e80f06513b8280862347d836
sucheng <2939428398@qq.com>
2022-04-12 18:49:15 +0800

    第二次提交

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: 苏成
+ * @Date: 2022/04/12/18:45
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello,git");
+    }
+}
